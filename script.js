73eba20bc70daa8a6ef8ac43776c260f303c2edb
Serious-Fin <augustasgri@gmail.com>
2023-07-17 22:40:18 +0300

    add gameController module to play in console

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const gameBoard = (() => {
 
     const makeMove = (player, row, column) => {
         // Stop execution if cell is already played
-        if (board[row][column] === " ") return;
+        if (board[row][column] !== " ") return;
 
         // Otherwise play cell
         board[row][column] = player.marker;
@@ -60,3 +60,47 @@ const gameBoard = (() => {
 
     return { getBoard, makeMove, checkWinner };
 })();
+
+const gameController = (() => {
+    const playerOne = playerFactory("X", "Player One");
+    const playerTwo = playerFactory("O", "Player Two");
+    let currentPlayer = playerOne;
+
+    const _changePlayer = () => {
+        if (currentPlayer === playerOne) {
+            currentPlayer = playerTwo;
+        }
+        else {
+            currentPlayer = playerOne;
+        }
+    };
+
+    const playTurn = () => {
+        console.log("Turn of " + currentPlayer.name);
+
+        const board = gameBoard.getBoard();
+
+        for (let i = 0; i < 3; i++)
+        {
+            console.log("'" + board[i][0] + "'" + board[i][1] + "'" + board[i][2] + "'");
+        }
+
+        let row = prompt("Enter row: ");
+        let column = prompt("Enter column: ");
+
+        gameBoard.makeMove(currentPlayer, row, column);
+
+        if (gameBoard.checkWinner() !== null) {
+            console.log("Winner is " + gameBoard.checkWinner());
+            return;
+        }
+
+        _changePlayer();
+
+        playTurn();0
+    };
+
+    return { playTurn };
+})();
+
+gameController.playTurn();
